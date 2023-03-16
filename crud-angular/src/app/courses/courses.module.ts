7ed0cc7f3f9ca9aaeb4c68f5a22ad25093a0e759
Alexandregv1080p <alexandregv1999@gmail.com>
2023-03-16 19:28:20 -0300

    nao e o ideal mas acontece

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,16 +1,22 @@
+import { CoursesComponent } from './courses/courses.component';
 import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CoursesRoutingModule } from './courses-routing.module';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CoursesComponent
+  ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
-
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
